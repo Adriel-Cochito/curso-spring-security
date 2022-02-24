6270d981d9e142881b699e06a3d634177911ae48
Adriel-Cochito <adriel.cochito@al.infnet.edu.br>
2022-02-24 01:09:32 -0300

    Inserindo consulta
    Inserindo consulta e bloqueando o horario para outros agendamentos

--- demo-security/src/main/java/com/mballem/curso/security/service/AgendamentoService.java
+++ demo-security/src/main/java/com/mballem/curso/security/service/AgendamentoService.java
@@ -1,4 +1,4 @@
-package com.mballem.curso.security.web.controller;
+package com.mballem.curso.security.service;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.mballem.curso.security.datatables.Datatables;
+import com.mballem.curso.security.domain.Agendamento;
 import com.mballem.curso.security.domain.Horario;
+import com.mballem.curso.security.web.controller.AgendamentoRepository;
 
 @Service
 public class AgendamentoService {
@@ -22,6 +24,12 @@ public class AgendamentoService {
 	public List<Horario> buscarHorariosNaoAgendadosPorMedicoIdEData(Long id, LocalDate data) {
 		
 		return repository.findByMedicoIdAndDataNotHorarioAgendado(id, data);
+	}
+
+	@Transactional(readOnly = false)
+	public void salvar(Agendamento agendamento) {
+		
+		repository.save(agendamento);
 	}
 
 }
--- demo-security/src/main/java/com/mballem/curso/security/web/controller/AgendamentoController.java
+++ demo-security/src/main/java/com/mballem/curso/security/web/controller/AgendamentoController.java
@@ -6,12 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.mballem.curso.security.domain.Agendamento;
+import com.mballem.curso.security.domain.Especialidade;
+import com.mballem.curso.security.domain.Paciente;
+import com.mballem.curso.security.service.AgendamentoService;
 import com.mballem.curso.security.service.EspecialidadeService;
 import com.mballem.curso.security.service.PacienteService;
 
@@ -39,6 +46,20 @@ public class AgendamentoController {
 			@PathVariable("data") @DateTimeFormat(iso = ISO.DATE) LocalDate data) {
 
 		return ResponseEntity.ok(service.buscarHorariosNaoAgendadosPorMedicoIdEData(id, data));
+	}
+
+	// salvar um consulta agendada
+	@PostMapping({ "/salvar" })
+	public String salvar(Agendamento agendamento, RedirectAttributes attr, @AuthenticationPrincipal User user) {
+		Paciente paciente = pacienteService.buscarPorUsuarioEmail(user.getUsername());
+		String titulo = agendamento.getEspecialidade().getTitulo();
+		Especialidade especialidade = especialidadeService.buscarPorTitulos(new String[] { titulo }).stream()
+				.findFirst().get();
+		agendamento.setEspecialidade(especialidade);
+		agendamento.setPaciente(paciente);
+		service.salvar(agendamento);
+		attr.addFlashAttribute("sucesso", "Sua consulta foi agendada com sucesso.");
+		return "redirect:/agendamentos/agendar";
 	}
 
 }
